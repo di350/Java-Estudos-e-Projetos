039b01c022556fa624a080714a8bffbedeebf7e1
Diego Martins <116931576+di350@users.noreply.github.com>
2026-01-21 22:20:46 -0300

    Create ExemploVariaveis.java

---
+++ fundamentos/variaveis/ExemploVariaveis.java
@@ -0,0 +1,23 @@
+public class ExemploVariaveis {
+
+    public static void main(String[] args) {
+
+        // Variável do tipo inteiro
+        int idade = 25;
+
+        // Variável do tipo decimal
+        double altura = 1.75;
+
+        // Variável do tipo texto
+        String nome = "Diego";
+
+        // Variável do tipo booleano
+        boolean estudante = true;
+
+        // Exibindo os valores no console
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("É estudante? " + estudante);
+    }
+}
